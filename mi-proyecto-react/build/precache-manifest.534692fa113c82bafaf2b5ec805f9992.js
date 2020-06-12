90bd746c1094b7d386304762b4a797acd1465c09
NicoGullino <43994767+NicoGullino@users.noreply.github.com>
2020-06-12 09:56:14 -0300

    Add files via upload

---
+++ mi-proyecto-react/build/precache-manifest.534692fa113c82bafaf2b5ec805f9992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61163472c6d2d47d2b5d518f4a437499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d55f9b64d7305d37c82b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ddb3ff1204d6dafa19ec",
+    "url": "/static/css/main.5b47777b.chunk.css"
+  },
+  {
+    "revision": "d55f9b64d7305d37c82b",
+    "url": "/static/js/2.34f69873.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/static/js/2.34f69873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb3ff1204d6dafa19ec",
+    "url": "/static/js/main.07c534f8.chunk.js"
+  },
+  {
+    "revision": "d148a626935890e27201",
+    "url": "/static/js/runtime-main.2d5105b8.js"
+  }
+]);
